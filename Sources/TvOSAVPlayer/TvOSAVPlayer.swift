6474cc141058d5e135c250185cedfa9568de4966
Ganesh Kumar <ganesh.kumar@sourcebits.com>
2020-05-21 13:06:44 +0530

    [FUT-SR-16]:- Added basic assets and XIB file

--- Sources/TvOSAVPlayer/TvOSAVPlayer.swift
+++ Sources/TvOSAVPlayer/TvOSAVPlayer.swift
@@ -1,7 +1,24 @@
-public struct TvOSAVPlayer {
+import Foundation
+import UIKit
+
+public class TvOSAVPlayer: UIView {
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var playOrPauseButton: UIButton!
+    @IBOutlet weak var fullScreenButton: UIButton!
+
     var text = "Hello, World!, Welcome to Swift Package Manager"
     
-    public init() {}
+    var fileURL: URL!
+    var enableControls: Bool = true
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     public func printHelloWorld() {
         print("\(text)")
